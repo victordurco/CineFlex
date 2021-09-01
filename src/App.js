3f8b290890023508690b5dd75ef6cd8fc22f2d16
victordurco <victordurco@gmail.com>
2021-09-01 15:26:54 -0300

    page starts with React Route

--- src/App.js
+++ src/App.js
@@ -1,14 +1,29 @@
 import './reset.css';
 import './App.css';
 import Header from './Components/Header';
-import PageTitle from './Components/PageTitle';
+import Home from './Pages/Home/Home';
+import React from "react";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route
+} from "react-router-dom";
+
 
 function App() {
   return (
-    <div className='app'>
-      <Header />
-      <PageTitle>Selecione o filme</PageTitle>
-    </div>
+    <Router>
+      <div className='app'>
+        <Header />
+        <Switch>
+          <Route path='/' exact>
+            <Home />
+          </Route>
+        </Switch>
+        
+      </div>
+    </Router>
+   
   );
 }
 
---
+++ src/Pages/Home/Home.js
@@ -0,0 +1,9 @@
+import PageTitle from "./PageTitle";
+
+export default function Home(){
+    return(
+        <>
+            <PageTitle>Selecione o filme</PageTitle>
+        </>
+    );
+}
