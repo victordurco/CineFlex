b30adc7d3cd122e81e2b268a97d19f43b93e4169
victordurco <victordurco@gmail.com>
2021-09-01 15:06:17 -0300

    page title done

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import './reset.css';
 import './App.css';
 import Header from './Components/Header';
+import PageTitle from './Components/PageTitle';
 
 function App() {
   return (
-    <Header />
+    <div className='app'>
+      <Header />
+      <PageTitle>Selecione o filme</PageTitle>
+    </div>
   );
 }
 
---
+++ src/Components/PageTitle.js
@@ -0,0 +1,7 @@
+import "./PageTitle.css";
+
+export default function PageTitle({children}){
+    return (
+        <div className='pageTitle'>{children}</div>
+    );
+}
